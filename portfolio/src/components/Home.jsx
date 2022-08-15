c0864df71bd3e2847082485bf3e8c5e7cdf381af
JubairRafi <jubairpial96960@gmail.com>
2022-08-15 23:22:41 +0600

    social icon set in nav bar. using react icon

---
+++ portfolio/src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {HiArrowNarrowRights} from 'react-icons/hi'
+export const Home = () => {
+  return (
+    <div className='w-full h-screen bg-[#0a192f]'>
+        
+    </div>
+  )
+}
