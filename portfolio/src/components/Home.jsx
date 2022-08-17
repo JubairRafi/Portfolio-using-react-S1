da8fdbc4e9a595e6aa2cf4c920c739be3b898197
JubairRafi <jubairpial96960@gmail.com>
2022-08-18 00:57:39 +0600

    link from react scroll in view work btn

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
+import { Link } from 'react-scroll';
 export const Home = () => {
   return (
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
@@ -11,7 +12,11 @@ export const Home = () => {
             <p className='text-[#8892b0] py-4 max-w-[700px]'>Love to learn new technology. I'm focused on building web applications.</p>
 
             <div>
-                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-green-600 hover:border-green-600'>View Work 
+                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-green-600 hover:border-green-600'>
+                  
+                  <Link to='work' smooth={true} duration={500}>
+                    View Work 
+                  </Link>
                   <span className='group-hover:rotate-90 group-hover:mb-2 duration-300'>
                       <HiArrowNarrowRight className='ml-3' />
                   </span>
